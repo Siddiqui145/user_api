d3bda77b4961a10049f95b21309d041f387b8a9c
Siddiqui Taha <tahasiddiqui541@gmail.com>
2024-12-15 12:39:18 +0530

    Designed a model for API Data

---
+++ lib/models/api_service.dart
@@ -0,0 +1,14 @@
+class Data{
+  final String gender;
+  final String email;
+  final String phone;
+  final String cell;
+
+  Data({
+    required this.gender,
+    required this.email,
+    required this.phone,
+    required this.cell,
+
+  });
+}

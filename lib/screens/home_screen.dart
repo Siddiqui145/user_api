77d0b8bfc22ba0cd6fecb4ce13b603fb73b1d292
Siddiqui Taha <tahasiddiqui541@gmail.com>
2024-12-15 13:15:18 +0530

    Fetching other details

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:user_api/models/api_service.dart';
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -10,7 +10,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<dynamic> users = [];
+  List<Data> users = [];
 
   @override
   void initState() {
@@ -25,8 +25,35 @@ class _HomeScreenState extends State<HomeScreen> {
     final body = response.body;
 
     final json = jsonDecode(body);
+    final results = json['results'] as List<dynamic>;
+
+    final transformed = results.map((e) {
+
+      final name = UserName(title: e['name']['title'] ?? 'N/A',
+       first: e['name']['first'] ?? 'N/A',
+        last: e['name']['last'] ?? 'N/A');
+
+        final address1 = Street(number: e['location']['street']['number'],
+         name: e['location']['street']['name']);
+
+      final address = Location(address: address1,
+       city: e['location']['city'],
+        country: e['location']['country'],
+         postcode: e['location']['postcode'],
+          state: e['location']['state']);
+
+      return Data(
+        email : e['email'],
+        gender: e['gender'],
+        phone: e['phone'],
+        cell: e['cell'],
+        name: name,
+        location: address,
+      );
+    }).toList();
+
     setState(() {
-      users = json['results'];
+      users = transformed;
     });
   }
 
@@ -41,15 +68,23 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: ListView.builder(itemCount: users.length, itemBuilder: (context, index) {
         final user = users[index];
-        final email = user['email'];
-        final gender = user['gender'];
-        final phone = user['phone'];
-        final cell = user['cell'];
+        final email = user.email;
+        final gender = user.gender;
+        final phone = user.phone;
+        final cell = user.cell;
+        final name = user.name.last;
+        final city = user.location.city;
         return ListTile(
           title: Text(email),
-          subtitle: Text(gender),
+          subtitle: Text(name),
           leading: Text(phone),
-          trailing: Text(cell),
+          trailing: Column(
+            children: [
+              Text(cell),
+              Text(city),
+            ],
+          ),
+          
         );
 
       }),
